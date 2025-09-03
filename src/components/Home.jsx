6c759cf2f5826e63fcde4d2396b35f6cdc3e46d2
Vasan <kjvasan28503@gmail.com>
2025-09-04 00:20:12 +0530

    work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="text">
           <div className="text-one">Hello, I'm</div>
           <div className="text-two">Vasan K J</div>
-          <div className="text-three">Computer Science student</div>
+          <div className="text-three">Engineer Trainee @ Sopra Steria</div>
           <div className="text-four">
             Having skills in Full Stack Web Development, Data Science, and
             Artificial Intelligence and Web Designing.
